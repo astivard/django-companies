af89bfb30dd7e74752c4dd3f7c163cf027109054
astivard <mdravitsa@mail.ru>
2022-02-13 01:24:45 +0300

    some fixes

--- itgomel/settings.py
+++ itgomel/settings.py
@@ -111,8 +111,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = []
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
