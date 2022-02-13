9b59f6335cb36cacadaf19160d4451ff4f7612a2
astivard <mdravitsa@mail.ru>
2022-02-13 21:49:32 +0300

    Some fixes

--- itgomel/settings.py
+++ itgomel/settings.py
@@ -113,7 +113,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = []
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
