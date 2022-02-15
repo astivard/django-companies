ccabad10741bfc2569602409af2321e711e1030a
astivard <mdravitsa@mail.ru>
2022-02-15 23:49:34 +0300

    Change model logo field

--- companies/admin.py
+++ companies/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.utils.safestring import mark_safe
 
 from .models import Company, Adress
 
@@ -10,21 +9,14 @@ class AdressInline(admin.TabularInline):
 
 
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'get_html_logo', 'is_visible')
+    list_display = ('id', 'name', 'is_visible')
     list_display_links = ('id', 'name')
     search_fields = ('name', 'conent')
     prepopulated_fields = {'slug': ("name",)}
 
     inlines = [AdressInline]
 
-    fields = ('name', 'slug', 'content', 'logo', 'get_html_logo', 'is_visible')
-    readonly_fields = ('get_html_logo', )
-
-    def get_html_logo(self, object):
-        if object.logo:
-            return mark_safe(f"<img src='{object.logo.url}' width=25>")
-
-    get_html_logo.short_description = "Логотип"
+    fields = ('name', 'slug', 'content', 'logo_url', 'is_visible')
 
 
 admin.site.register(Company, CompanyAdmin)
--- itgomel/settings.py
+++ itgomel/settings.py
@@ -128,8 +128,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
 
 CACHES = {
     'default': {
